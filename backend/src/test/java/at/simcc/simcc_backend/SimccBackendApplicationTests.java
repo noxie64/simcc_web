713cc32f50df106181530a5c45c48bda8eb6637d
noxie64 <the-real-nox@outlook.com>
2026-03-20 11:24:21 +0100

    Initial commit.

---
+++ backend/src/test/java/at/simcc/simcc_backend/SimccBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.simcc.simcc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimccBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
