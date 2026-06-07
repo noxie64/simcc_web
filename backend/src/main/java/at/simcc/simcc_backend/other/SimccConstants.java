09cc4adc8bc9248f414900121b3fe97540a30a1f
noxie64 <gkollegger5@outlook.com>
2026-06-07 16:48:24 +0200

    feat: added auto-init of docker-client.

--- backend/src/main/java/at/simcc/simcc_backend/other/SimccConstants.java
+++ backend/src/main/java/at/simcc/simcc_backend/other/SimccConstants.java
@@ -1,11 +1,11 @@
 package at.simcc.simcc_backend.other;
 
 import lombok.Data;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.nio.file.Path;
 
 /**
@@ -15,8 +15,17 @@ import java.nio.file.Path;
  */
 @Component
 @Data
+@ConfigurationProperties(prefix = "simcc")
 public class SimccConstants {
     public static final Path DATA_DIR = Path.of("/simcc_data");
     public static final Path TROJAN_DIR = DATA_DIR.resolve("simcc_trojan");
     public static final Path BUILD_DIR = DATA_DIR.resolve("build");
+
+    private Docker docker = new Docker();
+
+    @Data
+    public static class Docker {
+        private String host;
+        private boolean tls;
+    }
 }
---
+++ backend/src/main/java/at/simcc/simcc_backend/trojan_build/TrojanBuildService.java
@@ -0,0 +1,73 @@
+package at.simcc.simcc_backend.trojan_build;
+
+import at.simcc.simcc_backend.other.SimccConstants;
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectId;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.Duration;
+
+/**
+ * Project: backend
+ * Created by: Georg Kollegger
+ * Date: 6/6/26
+ */
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class TrojanBuildService {
+    private Git trojanGitRepo;
+    private final SimccConstants simccConstants;
+    private final DockerClient docker;
+
+    @PostConstruct
+    public void init() throws IOException, GitAPIException {
+        setupDirectories();
+    }
+
+    private void setupDirectories() throws IOException, GitAPIException {
+        if (!Files.exists(SimccConstants.DATA_DIR)) {
+            Files.createDirectory(SimccConstants.DATA_DIR);
+        }
+
+        if (Files.exists(SimccConstants.TROJAN_DIR)) {
+            trojanGitRepo = Git.open(SimccConstants.TROJAN_DIR.toFile());
+            ObjectId oldHead = trojanGitRepo.getRepository().resolve("HEAD");
+            trojanGitRepo.fetch().call();
+            ObjectId fetchHead = trojanGitRepo.getRepository().resolve("FETCH_HEAD");
+
+            if (!oldHead.equals(fetchHead)) {
+                log.info("Pulling changes from trojan upstream...");
+                trojanGitRepo.pull().call();
+                buildDockerImage();
+            }
+        } else {
+            trojanGitRepo = Git.cloneRepository()
+                    .setURI("https://github.com/noxie64/simcc_trojan")
+                    .setDirectory(SimccConstants.TROJAN_DIR.toFile())
+                    .call();
+        }
+
+        if (!Files.exists(SimccConstants.BUILD_DIR)) {
+            Files.createDirectories(SimccConstants.BUILD_DIR);
+        }
+    }
+
+    private void buildDockerImage() {
+
+
+    }
+}
